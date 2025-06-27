cff911f94fc46e135be7d659fd5e57d5bb8355fb
MT <yen.danylo@lll.kpi.ua>
2025-06-27 15:28:39 +0300

    fix: Correct django-prometheus middleware name

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_prometheus.middleware.PrometheusAfterAdminMiddleware',
+    'django_prometheus.middleware.PrometheusAfterMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
