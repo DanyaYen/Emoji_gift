a558fd2ec85d82a7b536a9b510f6b18e4920e375
MT <yen.danylo@lll.kpi.ua>
2025-06-27 16:00:58 +0300

    refactor: Consolidate all API URLs into core.urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,11 +1,23 @@
-# backend/core/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
-from .views import hello_world 
+
+from api.views import (
+    UserRegisterView,
+    StoreListView,
+    TransactionCreateView,
+    ProfileView,
+    GiftItemView,
+    PurchaseItemView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_world, name='hello'),
-    path('api/', include('api.urls')),
     path('metrics/', include('django_prometheus.urls')),
+    path('api/register/', UserRegisterView.as_view(), name='register'),
+    path('api/store/', StoreListView.as_view(), name='store-list'),
+    path('api/transactions/', TransactionCreateView.as_view(), name='create-transaction'),
+    path('api/profile/', ProfileView.as_view(), name='profile'),
+    path('api/purchase/', PurchaseItemView.as_view(), name='purchase-item'),
+    path('api/gift/', GiftItemView.as_view(), name='gift-item'),
 ]
